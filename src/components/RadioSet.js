edb6ad64b4db0f8f3fadbc3e69c9dde5b301c3ea
Kay <kayxn23@gmail.com>
2018-12-09 21:59:06 -0800

    trying to debug why the playlist wont get updated when a track moves from one playlist to other seems like rendering issue or issue in the reutrn function

--- src/components/RadioSet.js
+++ src/components/RadioSet.js
@@ -14,9 +14,45 @@ class RadioSet extends React.Component {
     }
   }
 
+//while we're runing thru the logic we dont want prevstate to change
   moveTrackToOtherPlaylist = (track) => {
-    console.log("test print track from Radioset", track );
+    this.setState ((prevState, currentProps) => {
+
+      console.log("currentProps", currentProps);
+      let newArrayMorning = [];
+      let newEveningArray = [];
+      let tmp = {};
+      // .map iterates thru track array and turns it into array of track titles
+      if (prevState.morningTracks.map(track2 => track2.title).includes(track)){
+     // move stuff from morning to evening
+     // find the index of the track we are looking for in the morning array
+     tmp = prevState.morningTracks.filter(track2 => track2.title === track)[0];
+     //...prevState.morningTracks] makes copy of array
+     newArrayMorning = prevState.morningTracks.filter(track2 => track2.title !== track);
+     // save that track in the tmp variable
+     // remove from the morning array the element at that index
+// create a new evening array by doing eveningArray = [tmpTrack] + prevState.eveningArray
+     newEveningArray = [tmp].concat([...prevState.eveningTracks]);
+    }
+    else {
+     tmp = prevState.eveningTracks.filter(track2 => track2.title === track)[0];
+     newEveningArray = prevState.eveningTracks.filter(track2 => track2.title !== track);
+     newArrayMorning = [tmp].concat([...prevState.morningTracks]);
+    }
+    console.log("mroning ",newArrayMorning);
+    console.log("evening ",newEveningArray);
+
+      return {
+         morningTracks : newArrayMorning,
+         eveningTracks : newEveningArray
+      };
+    });
+    console.log("test is this show ing?",this.state.morningTracks);
+
   }
+
+
+
 
   render() {
   return (
